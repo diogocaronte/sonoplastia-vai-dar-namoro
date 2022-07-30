7cdb5764a4e0c9d9eb01d552b7d2ade79ed2e84d
Leonardo Vargas <leu1607@hotmail.com>
2022-07-30 17:48:18 -0300

    feat: adicionando o meu do shortcuts

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+const soundsKeys = {
+  1: 'ui',
+  2: 'queIssoMeuFilho'
+}
+
 const sounds = {
   ui: new Audio('../assets/sons/ui.mp3'),
   queIssoMeuFilho: new Audio('../assets/sons/que-isso-meu-filho.mp3')
@@ -7,3 +12,18 @@ const handleSound = (soundId) => {
   sounds[soundId].currentTime = 0
   sounds[soundId].play()
 }
+
+function changeHiddenShortcuts() {
+  const shortcuts = document.getElementById('shortcuts')
+
+  shortcuts.classList.toggle('active')
+}
+
+document.addEventListener('keydown', (event) => {
+  const { key } = event
+  const idSound = soundsKeys[key]
+
+  if(idSound) {
+    handleSound(idSound)
+  } 
+})
