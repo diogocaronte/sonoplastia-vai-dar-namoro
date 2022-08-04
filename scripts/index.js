d3ea40a85a66306fc7b7ec046dde844bef9e543a
Diogo Caronte <diogo.caronte@gmail.com>
2022-08-03 22:40:21 -0300

    energy

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,98 @@
-const soundsKeys = {
-  1: 'ui',
-  2: 'queIssoMeuFilho'
+const sounds_shortcuts = new Map();
+sounds_shortcuts.set("q", 0);
+sounds_shortcuts.set("w", 1);
+sounds_shortcuts.set("e", 2);
+sounds_shortcuts.set("r", 3);
+sounds_shortcuts.set("t", 4);
+sounds_shortcuts.set("y", 5);
+sounds_shortcuts.set("u", 6);
+sounds_shortcuts.set("i", 7);
+sounds_shortcuts.set("o", 8);
+sounds_shortcuts.set("a", 9);
+sounds_shortcuts.set("s", 10);
+sounds_shortcuts.set("d", 11);
+sounds_shortcuts.set("f", 12);
+
+const assets_path = '../assets/sons/';
+const sounds = [
+  {
+    label: 'uuuuuuuuuuuuuuiiiii',
+    path: assets_path + 'ui.mp3'
+  }, {
+    label: 'que isso meu filho, calma',
+    path: assets_path + 'que-isso-meu-filho.mp3'
+  }, {
+    label: 'tomi',
+    path: assets_path + 'tomi.mp3'
+  }, {
+    label: 'eu não entendi oq você falou',
+    path: assets_path + 'eu-nao-entendi-o-que-ele-falou.mp3'
+  }, {
+    label: 'Iraaaaaa',
+    path: assets_path + 'irraa.mp3'
+  }, {
+    label: 'cavalo',
+    path: assets_path + 'cavalo.mp3'
+  }, {
+    label: 'esqueça tudo',
+    path: assets_path + 'esqueca-tudo.mp3'
+  }, /* {
+    label: 'não não não não',
+    path: assets_path + ''
+  }, */ {
+    label: 'olha ele aê',
+    path: assets_path + 'olha-ele-ae.mp3'
+  }, {
+    label: 'calma pae',
+    path: assets_path + 'calma-pae.mp3'
+  }, {
+    label: 'ueeeeeeepa',
+    path: assets_path + 'uepa.mp3'
+  }, {
+    label: 'é brincadeira',
+    path: assets_path + 'e-brincadeira-ein.mp3'
+  }, {
+    label: 'chegaaa',
+    path: assets_path + 'cheega.mp3'
+  }, /* {
+    label: 'demaaaais',
+    path: assets_path + ''
+  }, */ {
+    label: 'ele gostaaaaaa',
+    path: assets_path + 'ele-gosta.mp3'
+  }
+];
+
+const $soundsList = document.querySelector('[data-id="sounds"]');
+$soundsList.addEventListener('click', ({ target }) => {
+  if (target.dataset.type !== "sound-item") return;
+
+  handleSoundByIndex(
+    Number(target.dataset.id)
+  );
+});
+
+for (const [index, sound] of Object.entries(sounds)) {
+  $soundsList.insertAdjacentHTML('beforeend', `<button data-id="${index}" data-type="sound-item" type="button">${sound.label}</button>`);
 }
 
-const sounds = {
-  ui: new Audio('../assets/sons/ui.mp3'),
-  queIssoMeuFilho: new Audio('../assets/sons/que-isso-meu-filho.mp3')
-};
+const $shortcutList = document.querySelector('[data-id="shortcuts"]');
+for (const [key, value] of sounds_shortcuts) {
+  $shortcutList.insertAdjacentHTML('beforeend', `<li><span>${key}</span>${sounds[value].label}</li>`);
+}
 
-const handleSound = (soundId) => {
-  sounds[soundId].currentTime = 0
-  sounds[soundId].play()
+const _lookup_cached_sound = new Map();
+const handleSoundByIndex = (soundIndex) => {
+  const sound = sounds[soundIndex];
+
+  let sound_cache = _lookup_cached_sound.get(sound);
+  if (!sound_cache) {
+    sound_cache = new Audio(sound.path);
+    _lookup_cached_sound.set(sound, sound_cache);
+  }
+
+  sound_cache.currentTime = 0
+  sound_cache.play()
 }
 
 function changeHiddenShortcuts() {
@@ -19,11 +101,9 @@ function changeHiddenShortcuts() {
   shortcuts.classList.toggle('active')
 }
 
-document.addEventListener('keydown', (event) => {
-  const { key } = event
-  const idSound = soundsKeys[key]
+document.addEventListener('keydown', ({ key }) => {
+  const index = sounds_shortcuts.get(key);
+  if (index === undefined) return;
 
-  if(idSound) {
-    handleSound(idSound)
-  } 
+  handleSoundByIndex(index);
 })
